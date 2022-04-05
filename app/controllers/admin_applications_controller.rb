50bc1c50ebb278d66931aba6f759daf2907b3335
Will Medders <wmedders@gmail.com>
2022-04-05 19:22:39 -0400

    Feat: application approval makes pets not adoptable

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -4,16 +4,10 @@ class AdminApplicationsController < ApplicationController
     @pets = @application.pets
     if @application.application_pets.map {|ap| ap.approved }.all?(true)
       @application.update(status: "Approved")
+      @application.pets.each {|pet| pet.update(adoptable: false) }
     elsif
       @application.application_pets.map {|ap| ap.approved }.any?(false) && @application.application_pets.map {|ap| ap.approved }.any?(!nil)
         @application.update(status: "Rejected")
     end
-  end
-
-  def update
-    application = Application.find(params[:id])
-    application_pet = Pet.find(params[:pet_id]).application_pets.find {|ap| ap.application_id == application.id }
-    application_pet.update(approved: params[:approved])
-    redirect_to "/admin/applications/#{application.id}"
   end
 end
--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -2,4 +2,11 @@ class ApplicationPetsController < ApplicationController
   def create
 
   end
+
+  def update
+    application = Application.find(params[:id])
+    application_pet = Pet.find(params[:pet_id]).application_pets.find {|ap| ap.application_id == application.id }
+    application_pet.update(approved: params[:approved])
+    redirect_to "/admin/applications/#{application.id}"
+  end
 end
