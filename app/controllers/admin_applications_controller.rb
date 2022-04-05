86201534730ebe14f951d6ee99c89e221c562795
Will Medders <wmedders@gmail.com>
2022-04-04 21:04:38 -0400

    Feat: admin app show approves application if all pets are approved

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -2,7 +2,9 @@ class AdminApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
-
+    if @application.application_pets.map {|ap| ap.approved }.all?(true)
+      @application.update(status: "Approved")
+    end
   end
 
   def update
