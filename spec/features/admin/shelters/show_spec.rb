2f184a38d283ea45fdb5607a9e0d256edfa6950f
Joseph Downs <jos.w.downs@gmail.com>
2022-04-05 17:03:51 -0600

    Test: to see action required section

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -9,8 +9,15 @@ RSpec.describe "AdminShelters", type: :feature do
     @rascal = @shelter_3.pets.create(name: "Rascal", age: 3, adoptable: true, breed: "lab")
     @boo = @shelter_3.pets.create(name: "Boo", age: 1, adoptable: true, breed: "lab")
     @tugboat = @shelter_3.pets.create(name: "Tugboat", age: 6, adoptable: true, breed: "lab")
-    @Raintree = @shelter_3.pets.create(name: "Raintree", age: 5, adoptable: true, breed: "lab")
+    @raintree = @shelter_3.pets.create(name: "Raintree", age: 5, adoptable: true, breed: "lab")
     @tinker = @shelter_3.pets.create(name: "Tinker", age: 2, adoptable: false, breed: "lab")
+
+    @app1 = Application.create(name: "Joseph" , street: "Humboldt", city: "Denver", state: "CO", zip: 98764, description: "I like dogs" , status: "Pending")
+    @app2 = Application.create(name: "Will", street: "Sunburst", city: "Alanta", state: "GA", zip: 76536, description: "I'm cool as a cucumber", status: "Pending")
+
+    ApplicationPet.create(pet_id: @rascal.id, application_id: @app1.id, approved: false)
+    ApplicationPet.create(pet_id: @boo.id, application_id: @app1.id, approved: false)
+    ApplicationPet.create(pet_id: @raintree.id, application_id: @app2.id, approved: false)
   end
 
   describe "when I visit a shelters show page as an admin" do
@@ -40,6 +47,22 @@ RSpec.describe "AdminShelters", type: :feature do
       visit "/admin/shelters/#{@shelter_3.id}"
 
       expect(page).to have_content("4")
+    end
+
+    it "shows a section titled action required" do
+      visit "/admin/shelters/#{@shelter_3.id}"
+
+      expect(page).to have_content("Action Required")
+    end
+
+    it "list the names of all the pets that have pending applications for this shelter" do
+      visit "/admin/shelters/#{@shelter_3.id}"
+
+      within('#action') do
+        expect(page).to have_content(@rascal.name)
+        expect(page).to have_content(@boo.name)
+        expect(page).to have_content(@raintree.name)
+      end
     end
 
 end
