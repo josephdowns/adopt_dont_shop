bfd73136ed0e98b6a06d8364056bb1180ce382da
Joseph Downs <jos.w.downs@gmail.com>
2022-04-06 16:17:50 -0600

    Delete pry

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -4,7 +4,6 @@ class ApplicationPetsController < ApplicationController
   end
 
   def update
-    binding.pry
     application = Application.find(params[:id])
     application_pet = Pet.find(params[:pet_id]).application_pets.find {|ap| ap.application_id == application.id }
     application_pet.update(approved: params[:approved])
